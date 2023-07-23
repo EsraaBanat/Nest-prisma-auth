3799b31ce185662dab2e49c31635d8c700fb918c
EsraaBanat <esraa.bnt96@gmail.com>
2023-07-23 10:26:42 +0300

    Add Prisma

---
+++ prisma/migrations/20230722141508_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `clientId` VARCHAR(191) NULL,
+    `role` ENUM('admin', 'receptionist', 'va') NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_phone_key`(`phone`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/prisma.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
+
+// import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService extends PrismaClient implements OnModuleInit {
+//   constructor() {
+//     super();
+//   }
+
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async enableShutdownHooks(app: INestApplication) {
+//     this.$on('beforeExit', async () => {
+//       await app.close();
+//     });
+//   }
+// }
